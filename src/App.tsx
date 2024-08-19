c133eeee54139355a9b4319be1505c28ae15c822
kimtaejin3 <rlaxowls1316@naver.com>
2024-08-19 16:55:12 +0900

    design: ItemList를 감싸는 Container 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { CSSProperties } from "react";
 import ItemList from "./components/ItemList";
 
 const App: React.FC = () => {
   return (
-    <>
+    <div style={getContainerStyle()}>
       <ItemList />
-    </>
+      <ItemList />
+      <ItemList />
+      <ItemList />
+    </div>
   );
 };
+
+const getContainerStyle = (): CSSProperties => ({
+  height: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "20px",
+});
 
 export default App;
