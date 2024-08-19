acf05e9708b3156cd5d2a27ebab866fddaf0f5bc
kimtaejin3 <rlaxowls1316@naver.com>
2024-08-19 17:48:08 +0900

    feat: 컬럼 확장 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,181 @@
-import { CSSProperties } from "react";
-import ItemList from "./components/ItemList";
+import { CSSProperties, SetStateAction, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import {
+  DragDropContext,
+  Draggable,
+  DraggingStyle,
+  Droppable,
+  DropResult,
+  NotDraggingStyle,
+} from "react-beautiful-dnd";
+
+export const data = [
+  {
+    id: "1",
+    Task: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent.",
+    Due_Date: "25-May-2020",
+  },
+  {
+    id: "2",
+    Task: "Fix Styling",
+    Due_Date: "26-May-2020",
+  },
+  {
+    id: "3",
+    Task: "Handle Door Specs",
+    Due_Date: "27-May-2020",
+  },
+  {
+    id: "4",
+    Task: "morbi",
+    Due_Date: "23-Aug-2020",
+  },
+  {
+    id: "5",
+    Task: "proin",
+    Due_Date: "05-Jan-2021",
+  },
+];
+
+type ColumnsType = {
+  [x: string]: {
+    title: string;
+    items: { id: string; Task: string; Due_Date: string }[];
+  };
+};
+
+export const columnsFromBackend: ColumnsType = {
+  [uuidv4()]: {
+    title: "To-do",
+    items: data,
+  },
+  [uuidv4()]: {
+    title: "In Progress",
+    items: [],
+  },
+  [uuidv4()]: {
+    title: "Fail",
+    items: [],
+  },
+  [uuidv4()]: {
+    title: "Done",
+    items: [],
+  },
+};
 
 const App: React.FC = () => {
+  const [columns, setColumns] = useState(columnsFromBackend);
+
+  const onDragEnd = (
+    result: DropResult,
+    columns: ColumnsType,
+    setColumns: React.Dispatch<SetStateAction<ColumnsType>>
+  ) => {
+    const { source, destination } = result;
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems,
+        },
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems,
+        },
+      });
+    }
+  };
+
   return (
-    <div style={getContainerStyle()}>
-      <ItemList />
-      <ItemList />
-      <ItemList />
-      <ItemList />
-    </div>
+    <DragDropContext
+      onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+    >
+      <div style={getContainerStyle}>
+        {Object.entries(columns).map(([columId, column], index) => {
+          return (
+            <Droppable key={columId} droppableId={columId}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  style={getListStyle(snapshot.isDraggingOver)}
+                >
+                  {column.items.map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                          )}
+                        >
+                          {item.Task}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+              )}
+            </Droppable>
+          );
+        })}
+      </div>
+    </DragDropContext>
   );
 };
 
-const getContainerStyle = (): CSSProperties => ({
+const getContainerStyle: CSSProperties = {
   height: "100vh",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
   gap: "20px",
+};
+
+const GRID = 8;
+
+const getItemStyle = (
+  isDragging: boolean,
+  draggableStyle: DraggingStyle | NotDraggingStyle
+): CSSProperties => ({
+  userSelect: "none",
+  padding: GRID * 2,
+  margin: `0 0 ${GRID}px 0`,
+  background: isDragging ? "lightgreen" : "grey",
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver: boolean): CSSProperties => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: GRID,
+  width: 250,
+  minHeight: 500,
 });
 
 export default App;
